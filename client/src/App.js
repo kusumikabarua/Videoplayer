28660ac6a851583522115fce73f81395851351c1
Kusumika Barua <kusumikabarua@gmail.com>
2024-08-26 12:18:02 +0530

    Add. Video player Frontend

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+// This imports the functional component from the previous sample.
+import VideoJS from './components/VideoJS'
+import videojs from 'video.js';
+
+const App = () => {
+  const playerRef = React.useRef(null);
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [{
+      src: '//vjs.zencdn.net/v/oceans.mp4',
+      type: 'video/mp4'
+    }]
+  };
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on('waiting', () => {
+      videojs.log('player is waiting');
+    });
+
+    player.on('dispose', () => {
+      videojs.log('player will dispose');
+    });
+  };
+
+  return (
+    <>
+      <div>Rest of app here</div>
+      
+      <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+      <div>Rest of app here</div>
+    </>
+  );
+}
+
+export default App;
